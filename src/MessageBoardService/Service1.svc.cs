ec37898e0800ab8d381bca862d204ef72fb94227
Andrei Tenea <andrei.tenea93@gmail.com>
2017-01-17 22:40:56 +0200

    InsertNewUser method

--- src/MessageBoardService/Service1.svc.cs
+++ src/MessageBoardService/Service1.svc.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MessageBoardDAL;
+using MessageBoardDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -12,22 +14,37 @@ namespace MessageBoardService
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public void InsertNewUser(UserDTO user)
         {
-            return string.Format("You entered: {0}", value);
-        }
+            try
+            {
+                using (var context = new MessageBoardEntities())
+                {
+                    tblUser addUser = context.tblUsers.Create();
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
+                    addUser.FirstName = user.FirstName;
+                    addUser.LastName = user.LastName;
+                    addUser.Username = user.Username;
+                    addUser.PasswordHash = user.PasswordHash;
+
+                    addUser.AccountCreationDate = DateTime.Now;
+                    addUser.City = "Craiova";
+                    addUser.Country = "Romania";
+                    addUser.Function = "Programmer";
+                    addUser.IsActive = true;
+                    addUser.IsAdministrator = true;
+
+                    context.tblUsers.Add(addUser);
+                    context.SaveChanges();
+                }
+                
             }
-            if (composite.BoolValue)
+            catch (Exception)
             {
-                composite.StringValue += "Suffix";
+
+                throw;
             }
-            return composite;
+            
         }
     }
 }
