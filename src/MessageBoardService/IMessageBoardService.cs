5979891c0747645858f57b7d5e8649776b6b18d1
NRO\a.f.tenea <andrei.tenea93@gmail.com>
2017-03-03 17:40:08 +0200

    Change service name

---
+++ src/MessageBoardService/IMessageBoardService.cs
@@ -0,0 +1,34 @@
+﻿using MessageBoardDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MessageBoardService
+{
+    [ServiceContract]
+    public interface IMessageBoardService
+    {
+        [OperationContract]
+        void InsertNewUser(UserDTO user);
+        [OperationContract]
+        UserDTO CheckUserAndPassword(string username);
+        [OperationContract]
+        List<UserDTO> FillUsersGrid();
+        [OperationContract]
+        UserDTO GetUserDetails(int userID);
+        [OperationContract]
+        bool IsAdministrator(string username);
+        [OperationContract]
+        void ChangePassword(UserDTO user);
+        [OperationContract]
+        void UpdateIsActive(List<UserDTO> users);
+        [OperationContract]
+        void AddNewPost(List<string> addPost);
+        [OperationContract]
+        Dictionary<PostDTO, DateTime?> FillPostsGrid();
+    }
+}
