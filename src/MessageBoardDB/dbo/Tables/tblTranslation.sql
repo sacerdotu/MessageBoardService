117817f5add13b576a94bda27019b35f81e0bdd1
Andrei Tenea <andrei.tenea93@gmail.com>
2017-04-25 23:49:32 +0300

    Add tblTranslations

---
+++ src/MessageBoardDB/dbo/Tables/tblTranslation.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblTranslation] (
+    [TranslationID] INT           IDENTITY (1, 1) NOT NULL,
+    [FormName]      NVARCHAR (50) NULL,
+    [ControlName]   NVARCHAR (50) NULL,
+    [RowKey]        NVARCHAR (50) NULL,
+    CONSTRAINT [PK_tblTranslation] PRIMARY KEY CLUSTERED ([TranslationID] ASC)
+);
+
+
+
